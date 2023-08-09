e674d082e8a1176322bf3d833d3acf843f3eca27
RayverAimar <rayver.munoz@ucsp.edu.pe>
2023-08-08 20:43:42 -0500

    Handling different types of errors, rename flightScraper.py  * FlightScraper.py renamed into latam_scraper.py  * Rewrite latam_scraper class to hold TimeoutExceptions, StaleElementExceptions, ElementClickInterceptedExceptions and NoSuchelementExceptions.  * Avoid repetition of code while getting attributes of element  * Modify the name of output json file

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,8 @@ def get_minutes_from_str(datetime_str):
         If datetime lenght is more than 4 characters then it has minutes in it.
         Sample: 1 hr 25 min -> 25
         '''
+        if not datetime_str:
+            return None
         index_to_trim = -1
         for i, e in enumerate(datetime_str):
             if e == MINUTES[0]:
@@ -14,19 +16,21 @@ def get_minutes_from_str(datetime_str):
                 break
         return int(datetime_str[index_to_trim-3:index_to_trim-1])
 
-def get_hours_from_str(datetime_):
+def get_hours_from_str(datetime_str):
     '''
         Function to return hours of a formatted str. 
         Sample: 10 hr 25 min -> 1
                 2 hr 35 min -> 2
                 5 hr -> 5
     '''
+    if not datetime_str:
+        return None
     index_to_trim = -1
-    for i, e in enumerate(datetime_):
+    for i, e in enumerate(datetime_str):
         if e == HOUR[0]:
             index_to_trim = i
             break
-    return int(datetime_[:index_to_trim-1])
+    return int(datetime_str[:index_to_trim-1])
 
 def get_hours_and_minutes_from_time(datetime_):
     '''
@@ -42,40 +46,46 @@ def get_hours_and_minutes_from_time(datetime_):
     return int(datetime_[:separator_index]), int(datetime_[separator_index+1:])
 
 class Flight:
-    def __init__(self, fees, currency, duration, departure_datetime, arrival_datetime, scale=None, next_flight=None):
+    def __init__(self, fees, currency, duration, departure_datetime, arrival_datetime, scale):
         self.fees = fees
         self.currency = currency
         self.duration = duration
         self.departure_datetime = departure_datetime
         self.arrival_datetime = arrival_datetime
         self.scale = scale
-        self.next_flight = next_flight
         self.details = []
 
     def add_details(self, detail):
         self.details.append(detail)
-
-    def print_details(self):
-        for detail in self.details:
-            print(detail)
     
     def get_dict(self):
         flight_dict = {}
         fees_dict = {}
-        for fee in self.fees:
-            fees_dict[fee.name] = fee.price
+        if self.fees:
+            for fee in self.fees:
+                fees_dict[fee.name] = fee.price
         flight_dict['price'] = fees_dict
-        flight_dict['currency'] = self.currency.upper()
+        flight_dict['currency'] = self.currency
         flight_dict['duration'] = self.duration
-        flight_dict['departure_date'] = '-'.join((str(self.departure_datetime.year), str(self.departure_datetime.month), str(self.departure_datetime.day)))
-        flight_dict['departure_time'] = ':'.join((str(self.departure_datetime.hour), str(self.departure_datetime.minute)))
-        flight_dict['arrival_date'] = '-'.join((str(self.arrival_datetime.year), str(self.arrival_datetime.month), str(self.arrival_datetime.day)))
-        flight_dict['arrival_time'] = ':'.join((str(self.arrival_datetime.hour), str(self.arrival_datetime.minute)))
+        if self.departure_datetime:
+            flight_dict['departure_date'] = '-'.join((str(self.departure_datetime.year), str(self.departure_datetime.month), str(self.departure_datetime.day)))
+            flight_dict['departure_time'] = ':'.join((str(self.departure_datetime.hour), str(self.departure_datetime.minute)))
+        else:
+            flight_dict['departure_date'] = None
+            flight_dict['departure_time'] = None
+        
+        if self.arrival_datetime:
+            flight_dict['arrival_date'] = '-'.join((str(self.arrival_datetime.year), str(self.arrival_datetime.month), str(self.arrival_datetime.day)))
+            flight_dict['arrival_time'] = ':'.join((str(self.arrival_datetime.hour), str(self.arrival_datetime.minute)))
+        else:
+            flight_dict['arrival_date'] = None
+            flight_dict['arrival_time'] = None
         flight_dict['scales'] = self.scale
         details_list = []
         for detail in self.details:
-            details_list.append(detail.get_dict())
-        flight_dict['details'] = details_list
+            if detail:
+                details_list.append(detail.get_dict())
+        flight_dict['details'] = details_list if self.details else None
         return flight_dict
     
 
